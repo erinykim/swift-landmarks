72100145e516eb38f5eec7a0c494506436b0b1e3
Erin Kim <erinkim395@gmail.com>
2022-10-23 10:05:18 -0400

    add project files

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Erin Kim on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
